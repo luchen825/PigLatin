0ce06b2fb913e7d980f6bd76ecb42e78f69d34bc
unknown <luchen@697-334-Bilbo.sf.k12.internal>
2015-01-06 11:02:13 -0800

    words.txt finished

--- build-tmp/source/PigLatin.java
+++ build-tmp/source/PigLatin.java
@@ -1,8 +1,26 @@
-import java.util.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PigLatin extends PApplet {
+
+
 
 public void setup() {
 	String lines[] = loadStrings("words.txt");
-	//String liness[] = loadStrings("LowellHymn.txt");
+	String liness[] = loadStrings("LowellHymn.txt");
 	System.out.println("there are " + lines.length + " lines");
 	for (int i = 0 ; i < lines.length; i++) {
 	  System.out.println(pigLatin(lines[i]));
@@ -49,4 +67,13 @@ public String pigLatin(String sWord)
 	{
 		return "ERROR!";
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PigLatin" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
